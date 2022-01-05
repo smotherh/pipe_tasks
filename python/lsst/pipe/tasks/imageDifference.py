823fdc1ab4e72b78d45d9506e7c604f9541bdab3
Ian Sullivan <sullii@uw.edu>
2022-01-05 11:11:14 -0800

    Add preconvolution and score image output

--- python/lsst/pipe/tasks/imageDifference.py
+++ python/lsst/pipe/tasks/imageDifference.py
@@ -23,10 +23,14 @@ import numpy as np
 
 import lsst.afw.image
 import lsst.afw.math
+import lsst.afw.table
 import lsst.ip.diffim
+import lsst.obs.base
 import lsst.pex.config
 import lsst.pipe.base
 from lsst.pipe.base import connectionTypes
+
+from .measurePsf import MeasurePsfTask
 
 __all__ = ["AlardLuptonSubtractConfig", "AlardLuptonSubtractTask"]
 
@@ -55,6 +59,13 @@ class AlardLuptonSubtractConnections(lsst.pipe.base.PipelineTaskConnections,
         name="{fakesType}{coaddName}Diff_templateExp"
     )
 
+    scoreExposure = connectionTypes.Output(
+        doc="Output AL likelihood or Zogy score image",
+        dimensions=("instrument", "visit", "detector"),
+        storageClass="ExposureF",
+        name="{fakesType}{coaddName}Diff_scoreExp",
+    )
+
     difference = connectionTypes.Output(
         doc="Result of subtracting convolved template from science image.",
         dimensions=("instrument", "visit", "detector"),
@@ -70,6 +81,8 @@ class AlardLuptonSubtractConnections(lsst.pipe.base.PipelineTaskConnections,
 
     def __init__(self, *, config=None):
         super().__init__(config=config)
+        if not config.doWriteScoreExp:
+            self.outputs.remove("scoreExposure")
 
 
 class AlardLuptonSubtractConfig(lsst.pipe.base.PipelineTaskConfig,
@@ -94,6 +107,22 @@ class AlardLuptonSubtractConfig(lsst.pipe.base.PipelineTaskConfig,
         doc="Task to decorrelate the image difference.",
     )
 
+    doWriteScoreExp = lsst.pex.config.Field(
+        dtype=bool,
+        default=True,
+        doc="Write AL likelihood or Zogy score exposure?"
+    )
+
+    measureTemplatePsf = lsst.pex.config.Field(
+        dtype=bool,
+        default=True,
+        doc="Measure the PSF of the template before constructing the matching kernel."
+    )
+    measurePsf = lsst.pex.config.ConfigurableField(
+        target=MeasurePsfTask,
+        doc="Measure PSF",
+    )
+
     def setDefaults(self):
         # defaults are OK for catalog and diacatalog
 
@@ -101,6 +130,8 @@ class AlardLuptonSubtractConfig(lsst.pipe.base.PipelineTaskConfig,
         self.makeKernel.kernel.active.fitForBackground = True
         self.makeKernel.kernel.active.spatialKernelOrder = 1
         self.makeKernel.kernel.active.spatialBgOrder = 2
+
+        # self.measurePsf.starSelector.usesMatches = False
 
 
 class AlardLuptonSubtractTask(lsst.pipe.base.PipelineTask):
@@ -114,6 +145,9 @@ class AlardLuptonSubtractTask(lsst.pipe.base.PipelineTask):
         super().__init__(**kwargs)
         self.makeSubtask("decorrelate")
         self.makeSubtask("makeKernel")
+
+        self.schema = lsst.afw.table.SourceTable.makeMinimalSchema()
+        self.makeSubtask("measurePsf", schema=self.schema)
 
     @lsst.utils.inheritDoc(lsst.pipe.base.PipelineTask)
     def runQuantum(self, butlerQC: lsst.pipe.base.ButlerQuantumContext,
@@ -133,53 +167,35 @@ class AlardLuptonSubtractTask(lsst.pipe.base.PipelineTask):
         convolutionControl = lsst.afw.math.ConvolutionControl()
         convolutionControl.setDoNormalize(False)
 
+        preconvolvedScience, preconvolveKernel = self.preconvolveScience(science, convolutionControl)
+
+        # This could become a PipelineTask that produces kernelSources as output
+        kernelSources = self.makeKernel.selectKernelSources(template, preconvolvedScience,
+                                                            candidateList=sources,
+                                                            preconvolved=True)
+
+        if self.config.measureTemplatePsf:
+            templatePsfSize0 = self._getFwhmPix(template)
+            self.log.info("Template PSF size prior to measurement: %f", templatePsfSize0)
+            exposureIdInfo = lsst.obs.base.ExposureIdInfo()
+            self.measurePsf.run(exposure=template, sources=sources, matches=None,
+                                expId=exposureIdInfo.expId)
+
         sciencePsfSize = self._getFwhmPix(science)
         templatePsfSize = self._getFwhmPix(template)
         self.log.info("Science PSF size: %f", sciencePsfSize)
         self.log.info("Template PSF size: %f", templatePsfSize)
-
-        preconvolvedScience = self.preconvolveScience(science, convolutionControl)
-
-        # This should become a PipelineTask that produces kernelSources as output
-        kernelSources = self.makeKernel.selectKernelSources(template, preconvolvedScience,
-                                                            preconvolved=True)
-
         if sciencePsfSize < templatePsfSize:
             self.log.info("Template PSF size is the greater, convolving science image.")
             subtractRes = self.convolveScienceSubtract(template, science, kernelSources, convolutionControl)
         else:
             self.log.info("Science PSF size is the greater, convolving template image.")
             subtractRes = self.convolveTemplateSubtract(template, science, kernelSources, convolutionControl)
-
+        if self.config.doWriteScoreExp:
+            scoreExposure = self.preconvolveSubtract(template, preconvolvedScience, preconvolveKernel,
+                                                     kernelSources, convolutionControl)
+            subtractRes.scoreExposure = scoreExposure
         return subtractRes
-
-    def convolveTemplateSubtract(self, template, science, kernelSources, convolutionControl):
-        """Subtract a template from a science image using the Alard & Lupton (1998)
-        algorithm (convolving the template image with the science PSF).
-        """
-        kernel = self.makeKernel.run(template, science, kernelSources).psfMatchingKernel
-
-        # Science and template bboxes should be identical!
-        convolvedTemplate = lsst.afw.image.MaskedImageF(science.getBBox())
-        lsst.afw.math.convolve(convolvedTemplate, template.maskedImage, kernel, convolutionControl)
-
-        difference = lsst.afw.image.ExposureF(science, deep=True)
-        difference.maskedImage -= convolvedTemplate
-        difference.setWcs(science.getWcs())
-        difference.setFilterLabel(science.getFilterLabel())
-
-        matchedTemplate = self._makeExposure(convolvedTemplate, science)
-        if self.config.doDecorrelation:
-            # NOTE: This takes the unconvolved template!
-            difference = self.decorrelate.run(science, template, difference, kernel,
-                                              templateMatched=True,
-                                              preConvMode=False,
-                                              preConvKernel=None,
-                                              spatiallyVarying=False).correctedExposure
-        return lsst.pipe.base.Struct(difference=difference,
-                                     matchedTemplate=matchedTemplate,
-                                     matchedScience=science,
-                                     )
 
     def convolveScienceSubtract(self, template, science, kernelSources, convolutionControl):
         """Subtract a template from a science image using the Alard & Lupton (1998)
@@ -199,23 +215,87 @@ class AlardLuptonSubtractTask(lsst.pipe.base.PipelineTask):
 
         matchedScience = self._makeExposure(convolvedScience, science)
         if self.config.doDecorrelation:
+            self.log.info("Decorrelating image difference.")
             difference = self.decorrelate.run(matchedScience, template, difference, kernel,
                                               templateMatched=False,
                                               preConvMode=False,
                                               preConvKernel=None,
                                               spatiallyVarying=False).correctedExposure
+        else:
+            self.log.info("NOT decorrelating image difference.")
         return lsst.pipe.base.Struct(difference=difference,
                                      matchedTemplate=template,
                                      matchedScience=matchedScience,
                                      )
 
+    def convolveTemplateSubtract(self, template, science, kernelSources, convolutionControl):
+        """Subtract a template from a science image using the Alard & Lupton (1998)
+        algorithm (convolving the template image with the science PSF).
+        """
+        kernel = self.makeKernel.run(template, science, kernelSources).psfMatchingKernel
+
+        # Science and template bboxes should be identical!
+        convolvedTemplate = lsst.afw.image.MaskedImageF(science.getBBox())
+        lsst.afw.math.convolve(convolvedTemplate, template.maskedImage, kernel, convolutionControl)
+
+        difference = lsst.afw.image.ExposureF(science, deep=True)
+        difference.maskedImage -= convolvedTemplate
+        difference.setWcs(science.getWcs())
+        difference.setFilterLabel(science.getFilterLabel())
+
+        matchedTemplate = self._makeExposure(convolvedTemplate, science)
+        if self.config.doDecorrelation:
+            # NOTE: This takes the unconvolved template!
+            self.log.info("Decorrelating image difference.")
+            difference = self.decorrelate.run(science, template, difference, kernel,
+                                              templateMatched=True,
+                                              preConvMode=False,
+                                              preConvKernel=None,
+                                              spatiallyVarying=False).correctedExposure
+        else:
+            self.log.info("NOT decorrelating image difference.")
+        return lsst.pipe.base.Struct(difference=difference,
+                                     matchedTemplate=matchedTemplate,
+                                     matchedScience=science,
+                                     )
+
+    def preconvolveSubtract(self, template, preconvolvedScience, preconvolveKernel,
+                            kernelSources, convolutionControl):
+        """Subtract a template from a science image using the Alard & Lupton (1998)
+        algorithm (convolving the template image with the science PSF).
+        """
+        kernel = self.makeKernel.run(template, preconvolvedScience, kernelSources,
+                                     preconvolved=True).psfMatchingKernel
+
+        # Science and template bboxes should be identical!
+        convolvedTemplate = lsst.afw.image.MaskedImageF(preconvolvedScience.getBBox())
+        lsst.afw.math.convolve(convolvedTemplate, template.maskedImage, kernel, convolutionControl)
+
+        scoreExposure = lsst.afw.image.ExposureF(preconvolvedScience, deep=True)
+        scoreExposure.maskedImage -= convolvedTemplate
+        scoreExposure.setWcs(preconvolvedScience.getWcs())
+        scoreExposure.setFilterLabel(preconvolvedScience.getFilterLabel())
+
+        if self.config.doDecorrelation:
+            # NOTE: This takes the unconvolved template!
+            self.log.info("Decorrelating Score exposure.")
+            scoreExposure = self.decorrelate.run(preconvolvedScience, template, scoreExposure, kernel,
+                                                 templateMatched=True,
+                                                 preConvMode=True,
+                                                 preConvKernel=preconvolveKernel,
+                                                 spatiallyVarying=False).correctedExposure
+        else:
+            self.log.info("NOT decorrelating Score exposure.")
+        return scoreExposure
+
     def preconvolveScience(self, science, convolutionControl):
         # cannot convolve in place, so need a new image anyway
         convolvedScience = lsst.afw.image.MaskedImageF(science.getBBox())
         psfAvgPos = science.psf.getAveragePosition()
+        preconvolveKernel = science.psf.getLocalKernel(psfAvgPos)
         # convolve with science exposure's PSF model
         lsst.afw.math.convolve(convolvedScience, science.maskedImage,
-                               science.psf.getLocalKernel(psfAvgPos),
+                               preconvolveKernel,
                                convolutionControl)
         badPix = np.isnan(convolvedScience.image.array)
         if badPix is not None:
@@ -223,7 +303,7 @@ class AlardLuptonSubtractTask(lsst.pipe.base.PipelineTask):
             convolvedScience.image.array[badPix] = 0
             convolvedScience.mask.array[badPix] |= noData
 
-        return self._makeExposure(convolvedScience, science)
+        return self._makeExposure(convolvedScience, science), preconvolveKernel
 
     @staticmethod
     def _getFwhmPix(exposure):
